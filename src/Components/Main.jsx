08622def5a0ecfa492ab16eda305b70abe26151e
massimopallera <pallera.massimo@gmail.com>
2024-11-12 15:38:29 +0100

    added some style for body and card

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,13 +3,13 @@ export default function Main() {
     <main>
       <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col-12">
             <div className="card">
               <img src="https://via.placeholder.com/600x400" alt="Placeholder" className="card-img-top" />
               <div className="card-body">
-                <h5 className="card-title">Titolo del post</h5>
+                <h3 className="card-title">Titolo del Post</h3>
                 <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vel turpis ac neque volutpat iaculis.</p>
-                <a href="#" className="btn btn-primary">Leggi di più</a>
+                {/* <a href="#" className="btn btn-primary">Leggi di più</a> */}
              </div>
             </div>
           </div>
