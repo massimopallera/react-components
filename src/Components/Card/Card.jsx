51edb7db4745388b32d694ca3516ba7745e3b873
massimopallera <pallera.massimo@gmail.com>
2024-11-12 18:40:51 +0100

    made some changes on posts manipulation

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,18 +1,24 @@
 import './Card.css'
+import posts from '../../db/posts.js'
 
 export default function Card() {
+   
   return (
-
     <div className="col-12">
       <div className="card">
-        <img src="https://via.placeholder.com/600x400" alt="Placeholder" className="card-img-top" />
+        <img src={posts.img} alt="" className="card-img-top" />
         <div className="card-body">
-          <h3 className="card-title">Titolo del Post</h3>
-          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vel turpis ac neque volutpat iaculis.</p>
-          <a href="#" className="">Leggi di più</a>
+          <h3 className="card-title">
+            {posts.title}
+          </h3>
+          <p className="card-text">
+            {posts.content}
+          </p>
+          <a href="#" className="">
+            Leggi di più
+          </a>
         </div>
       </div>
     </div>
-
   )
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,18 +1,13 @@
+import Card from './Card/Card'
+
 export default function Main() {
   return (
     <main>
       <div className="container">
         <div className="row">
-          <div className="col-12">
-            <div className="card">
-              <img src="https://via.placeholder.com/600x400" alt="Placeholder" className="card-img-top" />
-              <div className="card-body">
-                <h3 className="card-title">Titolo del Post</h3>
-                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vel turpis ac neque volutpat iaculis.</p>
-                <a href="#" className="">Leggi di più</a>
-             </div>
-            </div>
-          </div>
+          
+          <Card />
+
         </div>
       </div>
     </main>
