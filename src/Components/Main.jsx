c2ed4e2fc658b8a9fe032046b0cac925228585ea
massimopallera <pallera.massimo@gmail.com>
2024-11-12 15:16:14 +0100

    created markup structure

---
+++ src/Components/Main.jsx
@@ -0,0 +1,20 @@
+export default function Main() {
+  return (
+    <main>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <img src="https://via.placeholder.com/600x400" alt="Placeholder" className="card-img-top" />
+              <div className="card-body">
+                <h5 className="card-title">Titolo del post</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vel turpis ac neque volutpat iaculis.</p>
+                <a href="#" className="btn btn-primary">Leggi di più</a>
+             </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
