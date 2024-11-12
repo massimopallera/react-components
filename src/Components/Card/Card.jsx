c434ac44dccd42104cc9870bad1b83a27019bc1b
massimopallera <pallera.massimo@gmail.com>
2024-11-12 16:06:15 +0100

    moved card component in a folder+its style

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import './Card.css'
+
+export default function Card() {
+  return (
+
+    <div className="col-12">
+      <div className="card">
+        <img src="https://via.placeholder.com/600x400" alt="Placeholder" className="card-img-top" />
+        <div className="card-body">
+          <h3 className="card-title">Titolo del Post</h3>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vel turpis ac neque volutpat iaculis.</p>
+          <a href="#" className="">Leggi di più</a>
+        </div>
+      </div>
+    </div>
+
+  )
+}
